5280f78771d6ee7fc13cdf7fd6e84fe21a295b5a
kauanmatos224 <kauanmatos294@gmail.com>
2023-07-08 15:41:17 -0300

    create project -configure dependencies

--- thot/src/App.vue
+++ thot/src/App.vue
@@ -1,8 +1,25 @@
 <template>
-  <router-view />
+  <div>
+    <div class="app-flex">
+      <Navigation />
+      <div class="app-content flex flex-column">
+        <router-view />
+      </div>
+
+    </div>
+  </div>
 </template>
 
- <style lang="scss">
+<script>
+import Navigation from "./components/Navigation";
+export default{
+  components: {
+    Navigation,
+  },
+}
+</script>
+
+<style lang="scss">
 @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap");
 
 * {
